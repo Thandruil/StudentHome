89b1c54154ddc166628f75e876d80d0cbd1f12c6
Erik Gaal <git@erikgaal.nl>
2015-06-09 02:46:27 +0200

    Basic implementation of households and inhabitants

---
+++ studenthome/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from studenthome.models import Household, Inhabitant
+
+
+@admin.register(Household)
+class HouseholdAdmin(ModelAdmin):
+    list_display = ('name', 'get_full_address')
+
+
+@admin.register(Inhabitant)
+class InhabitantAdmin(ModelAdmin):
+    list_display = ('__str__', 'household', 'room')
---
+++ studenthome/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Household(models.Model):
+    name = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    number = models.CharField(max_length=10)
+    postal = models.CharField(max_length=6)
+    city = models.CharField(max_length=50)
+
+    def get_full_address(self):
+        return '%s %s, %s' % (self.street, self.number, self.city)
+    get_full_address.short_description = 'Address'
+
+    def __str__(self):
+        return self.name
+
+
+class Inhabitant(models.Model):
+    user = models.OneToOneField(User)
+    household = models.ForeignKey('Household')
+    room = models.IntegerField()
+
+    def __str__(self):
+        return '%s' % (self.user.get_full_name())
