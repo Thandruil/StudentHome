069ebe09aa985afdd5bac3d4e114aa9ee22d2580
Erik Gaal <git@erikgaal.nl>
2015-06-12 15:49:49 +0200

    Added homepage with useless information

--- studenthome/urls.py
+++ studenthome/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
+
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
 ]
