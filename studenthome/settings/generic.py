b43a3f46529a122ba880b777ffe725ac718aacfd
Erik Gaal <git@erikgaal.nl>
2015-09-10 09:20:58 +0200

    Created Django app 'finance'

--- studenthome/settings/generic.py
+++ studenthome/settings/generic.py
@@ -6,6 +6,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'household',
+    'finance',
 )
 
 MIDDLEWARE_CLASSES = (
--- studenthome/urls.py
+++ studenthome/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -23,4 +22,5 @@ urlpatterns = [
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
 
     url(r'^', include('household.urls')),
+    url(r'^finance/', include('finance.urls')),
 ]
